05d5d426c7aa9da0a3e96a0947e5c306b8fc30c9
alexandrbogdanov <al1029ex@gmail.com>
2024-08-11 12:53:27 +0200

    conftest amended

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,8 +15,3 @@ def envs():
 def app_url():
     return os.getenv("APP_URL")
 
-
-def fetch_response(url, expected_status=HTTPStatus.OK):
-    response = requests.get(url)
-    assert response.status_code == expected_status, f"Failed to fetch data from {url}. Expected status {expected_status}, got {response.status_code}"
-    return response.json() if response.status_code == HTTPStatus.OK else response
